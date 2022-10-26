0a4a86eb1e0725f10e884390946d4db13c61a5fd
JacquieSue <112510142+JacquieSue@users.noreply.github.com>
2022-10-25 22:06:15 -0700

    removed results-html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,40 @@
 // ready to use w/ API key- http://api.openweathermap.org/geo/1.0/zip?zip=E14,GB&appid={86c03a249512cef92213348475754024}
 //api.openweathermap.org/data/2.5/weather?q={city name}&appid={86c03a249512cef92213348475754024}
 //var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + 86c03a249512cef92213348475754024;
+//{lon: -117.1573, lat: 32.7153}
 
-fetch(queryURL)
-var List = $('#cities-list');
-
+var cityWeather = {}
 var city;
 
 //make button into a variable
-var searchButton = document.getElementById("btn btn-info btn-block");
+var searchButton = document.getElementById("searchButton");
+var cityName = document.getElementById("cityName");
+
+
+var formEl = $('#search-form');
+var listEl = $('#cities-list');
+
+
+  // select form element by its `name` attribute and get its value
+  var newCity = $('input[name="city-input"]').val();
+
+
+function searchCity(event){
+    event.preventDefault();
+    //add localStorage to the Cities-list
+   var city = document.getElementById("search-input").value
+   var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=86c03a249512cef92213348475754024";
+   
+fetch(queryURL).then(weatherResponse =>{
+    console.log(weatherResponse)
+    if( weatherResponse.ok){
+        weatherResponse.json().then(weatherData =>{
+            console.log(weatherData)
+            cityName.textContent = weatherData.name;
+        })
+    }
+})
+}
 
 //add eventListener to search button 
-searchButton.addEventListener("click", function(event){
-    event.preventDefault;
-
-    //add localStorage to the Cities-list
-    var cityWeather = {}
-})
-
-var today = moment();
-$('').text(today.format("MM-DD-YYYY"));
+searchButton.addEventListener("click", searchCity);
