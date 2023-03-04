199a80d422d22132590341488af2d0b5c827b3be
JacquieSue <112510142+JacquieSue@users.noreply.github.com>
2023-03-03 21:32:36 -0700

    updated README.md

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
 # Weather-Dashboard
 Server-Side APIs Challenge
 
-## User Story
+## Description
+As a web developer, I want to showcase potential employers my ability to create a fully funtional weather dashboard
 
-As a web developer, I want to showcase potential employers my ability to create a fully funtional weather dahsboard
+## Table of Contents 
 
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Usage
 WHEN you open the webpage, a box will appear with the name of a city, the current date and an emoji displaying the expected weather (sunny, rainy, cloudy, etc.), temperature, wind, and humidity
 
 THEN columns can be seen underneath the box displaying the 5-day forecast for that city
@@ -13,7 +19,7 @@ WHEN you search for a new city to left side of the screen
 
 THEN  a list appears underneath the search box of the past cities that have been searched for
 
-## Usage
+![Screenshot](assets/image/Screen%20Shot%202022-10-25%20at%2010.18.00%20PM.png)
 
 FIRST make a header with "Weather Dashboard"
 
@@ -25,7 +31,11 @@ THEN specify the city's that are going to be used
 
 THEN  connect the city's elements to local storage
 
- ![weather dashboard](assets/image/Screen Shot 2022-10-25 at 10.18.00 PM.png)
+[View deployed website](https://jacquiesue.github.io/Weather-Dashboard/)
 
 ## Credits
+Jacquelyn Pulliam
+[GitHub](https://github.com/JacquieSue)
+
 ## License
+MIT
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 // ready to use w/ API key- http://api.openweathermap.org/geo/1.0/zip?zip=E14,GB&appid={86c03a249512cef92213348475754024}
 //api.openweathermap.org/data/2.5/weather?q={city name}&appid={86c03a249512cef92213348475754024}
 //var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + 86c03a249512cef92213348475754024;
-//{lon: -117.1573, lat: 32.7153}
+// {lon: -117.1573, lat: 32.7153}
 
 var cityWeather = {}
 var city;
@@ -9,31 +9,124 @@ var city;
 //make button into a variable
 var searchButton = document.getElementById("searchButton");
 var cityName = document.getElementById("cityName");
+var cityDate = document.getElementById("cityDate");
+var weatherTemp = document.getElementById("weatherTemp");
+var weatherEmoji = document.getElementById("weatherEmoji");
+var weatherWind = document.getElementById("weatherWind");
+var weatherHum = document.getElementById("weatherHum");
+var pastSearches = [];
 
 
 var formEl = $('#search-form');
 var listEl = $('#cities-list');
 
 
-  // select form element by its `name` attribute and get its value
-  var newCity = $('input[name="city-input"]').val();
+// select form element by its `name` attribute and get its value
+//   var newCity = $('input[name="city-input"]').val();
 
-function searchCity(event){
+function searchCity(event) {
     event.preventDefault();
-    //add localStorage to the Cities-list
-   var city = document.getElementById("search-input").value
-   var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=86c03a249512cef92213348475754024";
-   
-fetch(queryURL).then(weatherResponse =>{
-    console.log(weatherResponse)
-    if(weatherResponse.ok){
-        weatherResponse.json().then(weatherData =>{
-            console.log(weatherData)
-            cityName.textContent = weatherData.name;
-        })
-    }
-})
-}
 
+    //add localStorage to the Cities-list
+    var city = document.getElementById("search-input").value
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=86c03a249512cef92213348475754024";
+
+    fetch(queryURL)
+        .then(function (weatherResponse) {
+            // console.log(weatherResponse)
+            if (weatherResponse.ok) {
+                weatherResponse.json().then(function (weatherData) {
+                    // console.log(weatherData)
+                    let lat = weatherData.coord.lat;
+                    let lon = weatherData.coord.lon;
+                    forecast(lat, lon);
+                    // call history function here
+                    citySearch(city)
+                })
+            }
+        })
+}
+// FOCUS ON GETTING CITY NAME/DATE/TEMP/HUMIDITY/ICON/WIND SPEED
+//CHANGE HTTP TO HTTPS
+function forecast(lat, lon) {
+    var place = "https://api.openweathermap.org/data/2.5/forecast/?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=86c03a249512cef92213348475754024";
+    fetch(place)
+        .then(function (newCity) {
+            if (newCity.ok) {
+                newCity.json().then(function (cityData) {
+                    console.log(cityData)
+                    let cityName = cityData.city.name
+            })
+            }
+        })
+}
+function forecast(lat, lon) {
+    var place = "https://api.openweathermap.org/data/2.5/forecast/?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=86c03a249512cef92213348475754024";
+    fetch(place)
+        .then(function (getDate) {
+            if (getDate.ok) {
+                getDate.json().then(function (cityData) {
+                    console.log(cityData)
+                    let cityDate = cityData.list.dt
+            })
+            }
+        })
+}
+function forecast(lat, lon) {
+    var place = "https://api.openweathermap.org/data/2.5/forecast/?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=86c03a249512cef92213348475754024";
+    fetch(place)
+        .then(function (getTemp) {
+            if (getTemp.ok) {
+                getTemp.json().then(function (cityData) {
+                    console.log(cityData)
+                    let weatherTemp = cityData.list.main.temp_max
+            })
+            }
+        })
+}
+function forecast(lat, lon) {
+    var place = "https://api.openweathermap.org/data/2.5/forecast/?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=86c03a249512cef92213348475754024";
+    fetch(place)
+        .then(function (getHum) {
+            // console.log(newCity)
+            if (getHum.ok) {
+                getHum.json().then(function (cityData) {
+                    console.log(cityData)
+                    let weatherHum = cityData.list.main.humidity
+            })
+            }
+        })
+}
+function forecast(lat, lon) {
+    var place = "https://api.openweathermap.org/data/2.5/forecast/?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=86c03a249512cef92213348475754024";
+    fetch(place)
+        .then(function (getWind) {
+            if (getWind.ok) {
+                getWind.json().then(function (cityData) {
+                    console.log(cityData)
+                    let weatherWind = cityData.list.wind.speed
+            })
+            }
+        })
+}
+function forecast(lat, lon) {
+    var place = "https://api.openweathermap.org/data/2.5/forecast/?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=86c03a249512cef92213348475754024";
+    fetch(place)
+        .then(function (weatherIcon) {
+            if (weatherIcon.ok) {
+                weatherIcon.json().then(function (cityData) {
+                    console.log(cityData)
+                    let icon = cityData.list.weather.icon 
+            })
+            }
+        })
+} 
+// local storage
+                // sending info to local storage
+                function citySearch(cityName){
+pastSearches.push(cityName)
+console.log(pastSearches)
+                }
+                
 //add eventListener to search button 
-searchButton.addEventListener("click", searchCity);
+searchButton.addEventListener("click", searchCity)
